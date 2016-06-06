6c8e86d21f9fc48bbbde28175d78d2f2f3b8ebaa
pastoriza57 <pastoriza57@pastoriza57-PC>
2016-06-06 22:45:58 +0200

    proyecto

---
+++ ProyectoDiana/src/proyectodiana/ProyectoDiana.java
@@ -0,0 +1,16 @@
+package proyectodiana;
+
+
+
+public class ProyectoDiana {
+
+    public static void main(String[] args) {
+        Menu mn = new Menu();
+        mn.setVisible(true);
+        
+}
+}
+        
+    
+    
+
